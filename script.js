6d6bd1496a9aa3737fbb26f2c7275a1b0d16d4bf
Ornelio Mahingue <70206383+orneliomahingue@users.noreply.github.com>
2026-03-06 16:16:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+// animação simples ao clicar no menu
+
+document.querySelectorAll("nav a").forEach(link => {
+
+    link.addEventListener("click", function(e){
+
+        e.preventDefault()
+
+        const id = this.getAttribute("href")
+
+        document.querySelector(id).scrollIntoView({
+            behavior:"smooth"
+        })
+
+    })
+
+})
